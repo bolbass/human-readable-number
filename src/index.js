c4861305d7f6bbe044159102c60e2c2725cc50f5
bolbass <ann.bolbass@gmail.com>
2022-08-12 14:39:16 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let firstClass = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    let secondClass = [
+        "zero",
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (number < 20) {
+        return firstClass[number];
+    }
+    if (number < 100) {
+        let separation = Math.floor(number / 10);
+        return (
+            secondClass[separation] + (number % 10 === 0 ? "" : ` ${toReadable(number % 10)}`)
+        );
+    }
+    let hundred = Math.floor(number / 100);
+    return (
+        toReadable(hundred) +" hundred" + (number % 100 === 0 ? "" : ` ${toReadable(number % 100)}`)
+    );
+};
